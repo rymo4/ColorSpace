d3212c087a0ae8fddf05661d926422f342fa69ea
Ryder Moody <rydthemoodster@gmail.com>
2013-05-10 18:37:34 -0400

    Adjust keep and remove old code

--- public/level.js
+++ public/level.js
@@ -1,82 +1,34 @@
 window.CS = window.CS || {};
 CS.level1 = CS.level1 || {};
 
-CS.level1.platforms = [
-  /*{x: 0, y: -10},
-  {x: 1, y: -10},
-  {x: 2, y: -10},
-  {x: 3, y: -10},
-  {x: 4, y: -10},
-  {x: 5, y: -10},
-  {x: 6, y: -10},
-  {x: 7, y: -10},
-  {x: 8, y: -10},
-  {x: 9, y: -10},
-  {x: 10, y: -10},
-  {x: 11, y: -10},
-
-  {x: 10, y: -7},
-  {x: 9, y: -7},
-  {x: 8, y: -7},
-  {x: 7, y: -7},
-  {x: 6, y: -7},
-  {x: 5, y: -7},
-  {x: 11, y: -7},
-  {x: 12, y: -7},
-  {x: 13, y: -7},
-  {x: 14, y: -7},
-  {x: 15, y: -7},
-
-  {x: 12, y: -4},
-  {x: 13, y: -4},
-  {x: 14, y: -4},
-  {x: 15, y: -4},
-
-  {x: 0, y: 0},
-  {x: 1, y: 0},
-  {x: 2, y: 0},
-  {x: 3, y: 0},
-  {x: 4, y: 0},
-  {x: 5, y: 0},
-  {x: 6, y: 0},
-
-  {x: 15, y: 0},
-  {x: 16, y: 0},
-  {x: 17, y: 0},
-  {x: 18, y: 0},
-  {x: 19, y: 0},
-  {x: 20, y: 0},
-
-  {x: 10, y: 3},
-  {x: 11, y: 3},
-  {x: 12, y: 3},
-  {x: 13, y: 3},
-  {x: 14, y: 3},
-  {x: 15, y: 3}*/
-];
-CS.level1.tree = function(base_x, base_y){
+CS.level1.platforms = [];
+CS.level1.makeTree = function(base_x, base_y){
+  var height = Math.floor((Math.random() * 2) + 2);
   var a = [
-    {x: base_x, y: base_y},
-    {x: base_x, y: (base_y + 1)},
-    {x: base_x, y: (base_y + 2)},
-    {x: (base_x - 1), y: (base_y + 2)},
-    {x: (base_x + 1), y: (base_y + 2)}
+    {x: (base_x - 1), y: (base_y + height)},
+    {x: (base_x + 1), y: (base_y + height)}
   ];
-  if (Math.random() < 0.5){
-    a.push({x: base_x, y: base_y + 3});
+  for (var i = 0; i < height; i += 1){
+    a.push({x: (base_x), y: (base_y + i)});
   }
   if (Math.random() < 0.5){
-    a.push({x: base_x + 1, y: base_y + 3});
+    a.push({x: base_x + 1, y: base_y + height - 1});
   }
   if (Math.random() < 0.5){
-    a.push({x: base_x - 1, y: base_y + 3});
+    a.push({x: base_x - 1, y: base_y + height - 1});
+  }
+  if (Math.random() < 0.5){
+    a.push({x: base_x, y: base_y + height + 1});
   }
   return a;
 };
 
 CS.level1.makeKeep = function(x, y){
   var keep = [
-    "]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]          ]]]]]]]]]]]]]",
+    "                                            ]]]]]]]              ",
+    "                                           ]       ]             ",
+    "                                          ]         ]            ",
+    "]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]         ]]]]]]]]]]]]]",
     "]                                                               ]",
     "]                                                               ]",
     "]                                                               ]",
@@ -115,7 +67,7 @@ CS.level1.create = function(){
     CS.level1.platforms.push({x: i, y: -15});
     var rand = Math.random();
     if (rand < 0.2){
-      var tree = CS.level1.tree(i, -14);
+      var tree = CS.level1.makeTree(i, -14);
       for (var j = 0; j < tree.length; j += 1){
         CS.level1.platforms.push(tree[j]);
       }
